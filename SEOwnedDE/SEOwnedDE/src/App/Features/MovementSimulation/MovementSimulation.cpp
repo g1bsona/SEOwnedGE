9c127433e066e131de805e3e2711e8f142f0faba
Tsuki321 <tsukinokojima123@gmail.com>
2025-04-20 13:03:16 +0800

    Fixed warnings

--- SEOwnedDE/SEOwnedDE/src/App/Features/MovementSimulation/MovementSimulation.cpp
+++ SEOwnedDE/SEOwnedDE/src/App/Features/MovementSimulation/MovementSimulation.cpp
@@ -195,14 +195,16 @@ void CMovementSimulation::SetupMoveData(C_TFPlayer* pPlayer, CMoveData* pMoveDat
 		}
 		
 		// Handle specific TF2 conditions that affect movement
-		if (pPlayer->m_nPlayerCond() & (1 << TF_COND_SPEED_BOOST))
+		if (pPlayer->InCond(TF_COND_SPEED_BOOST))
 		{
 			movementMultiplier *= 1.2f;
 			isSpecialState = true;
 		}
 		
-		// TF_COND_SLOWED doesn't exist, using slowed effect from speed penalty
-		if (pPlayer->m_nPlayerCond() & (1 << TF_COND_SPEED_BOOST + 1))
+		// TF_COND_SLOWED doesn't exist, using a different approach for slow effects
+		// Instead of using (1 << TF_COND_SPEED_BOOST + 1) which causes overflow
+		// Look for other conditions that indicate slowdown
+		if (pPlayer->InCond(TF_COND_STUNNED) || pPlayer->m_flStunLerpTarget() > 0.0f || pPlayer->m_bStunNeedsFadeOut())
 		{
 			movementMultiplier *= 0.75f;
 			isSpecialState = true;
